1f89e169e49f26b16b504a5936d8c330e9c3fe82
tarloned <38500879+tarloned@users.noreply.github.com>
2021-01-19 20:26:57 +0300

    добавил файлы

--- weather/weather/ReadTemplates.cpp
+++ weather/weather/ReadTemplates.cpp
@@ -24,40 +24,162 @@ int readAllGreets(char buf[][512])
 
 int readAllDate(char buf[][512])
 {
-	return 0;
+	FILE* greetIn = fopen("Date.txt", "r");
+
+	int bufc = 0;
+	while (!feof(greetIn))
+	{
+		char line[512] = { 0 };
+		fgets(line, 512, greetIn);
+		line[strlen(line) - 1] = 0;
+		strcpy(buf[bufc++], line);
+	}
+
+	fclose(greetIn);
+
+	return bufc - 1;
 }
 
 int readAllTemp(char buf[][512])
 {
-	return 0;
+	FILE* greetIn = fopen("Temp.txt", "r");
+
+	int bufc = 0;
+	while (!feof(greetIn))
+	{
+		char line[512] = { 0 };
+		fgets(line, 512, greetIn);
+		line[strlen(line) - 1] = 0;
+		strcpy(buf[bufc++], line);
+	}
+
+	fclose(greetIn);
+
+	return bufc - 1;
 }
 
 int readAllTempNight(char buf[][512])
 {
-	return 0;
+	FILE* greetIn = fopen("TempNight.txt", "r");
+
+	int bufc = 0;
+	while (!feof(greetIn))
+	{
+		char line[512] = { 0 };
+		fgets(line, 512, greetIn);
+		line[strlen(line) - 1] = 0;
+		strcpy(buf[bufc++], line);
+	}
+
+	fclose(greetIn);
+
+	return bufc - 1;
+}
+
+int readAllReactions(char buf[][512])
+{
+	FILE* greetIn = fopen("Reactions.txt", "r");
+
+	int bufc = 0;
+	while (!feof(greetIn))
+	{
+		char line[512] = { 0 };
+		fgets(line, 512, greetIn);
+		line[strlen(line) - 1] = 0;
+		strcpy(buf[bufc++], line);
+	}
+
+	fclose(greetIn);
+
+	return bufc - 1;
 }
 
 int readAllOsadki(char buf[][512])
 {
-	return 0;
+	FILE* greetIn = fopen("Osadki.txt", "r");
+
+	int bufc = 0;
+	while (!feof(greetIn))
+	{
+		char line[512] = { 0 };
+		fgets(line, 512, greetIn);
+		line[strlen(line) - 1] = 0;
+		strcpy(buf[bufc++], line);
+	}
+
+	fclose(greetIn);
+
+	return bufc - 1;
 }
 
 int readAllPressure(char buf[][512])
 {
-	return 0;
+	FILE* greetIn = fopen("Pressure.txt", "r");
+
+	int bufc = 0;
+	while (!feof(greetIn))
+	{
+		char line[512] = { 0 };
+		fgets(line, 512, greetIn);
+		line[strlen(line) - 1] = 0;
+		strcpy(buf[bufc++], line);
+	}
+
+	fclose(greetIn);
+
+	return bufc - 1;
 }
 
 int readAllWind(char buf[][512])
 {
-	return 0;
+	FILE* greetIn = fopen("Wind.txt", "r");
+
+	int bufc = 0;
+	while (!feof(greetIn))
+	{
+		char line[512] = { 0 };
+		fgets(line, 512, greetIn);
+		line[strlen(line) - 1] = 0;
+		strcpy(buf[bufc++], line);
+	}
+
+	fclose(greetIn);
+
+	return bufc - 1;
 }
 
 int readAllJawlenie(char buf[][512])
 {
-	return 0;
+	FILE* greetIn = fopen("Jawlenie.txt", "r");
+
+	int bufc = 0;
+	while (!feof(greetIn))
+	{
+		char line[512] = { 0 };
+		fgets(line, 512, greetIn);
+		line[strlen(line) - 1] = 0;
+		strcpy(buf[bufc++], line);
+	}
+
+	fclose(greetIn);
+
+	return bufc - 1;
 }
 
 int readAllBye(char buf[][512])
 {
-	return 0;
+	FILE* greetIn = fopen("Bye.txt", "r");
+
+	int bufc = 0;
+	while (!feof(greetIn))
+	{
+		char line[512] = { 0 };
+		fgets(line, 512, greetIn);
+		line[strlen(line) - 1] = 0;
+		strcpy(buf[bufc++], line);
+	}
+
+	fclose(greetIn);
+
+	return bufc - 1;
 }
--- weather/weather/ReadTemplates.h
+++ weather/weather/ReadTemplates.h
@@ -4,7 +4,7 @@ int readAllGreets(char buf[][512]);
 int readAllDate(char buf[][512]);
 int readAllTemp(char buf[][512]);
 int readAllTempNight(char buf[][512]);
-int readAllReactionsTemp(char buf[][512]);
+int readAllReactions(char buf[][512]);
 int readAllOsadki(char buf[][512]);
 int readAllPressure(char buf[][512]);
 int readAllWind(char buf[][512]);
